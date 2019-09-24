166e99542f97ddc8f5f60959831a655dc4ec6e04
phamngoctan <pnt.dtqn1991@gmail.com>
2019-09-24 23:58:57 +0700

    Commit the missing part - yieldd()

--- src/test/java/com/tanpham/playaround/thread/YieldExample.java
+++ src/test/java/com/tanpham/playaround/thread/YieldExample.java
@@ -1,5 +1,26 @@
 package com.tanpham.playaround.thread;
 
-public class YieldExample {
+//MyThread extending Thread 
+class MyThread extends Thread {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(Thread.currentThread().getName() + " in control");
+		}
+	}
+}
 
+public class YieldExample {
+	public static void main(String[] args) {
+		MyThread t = new MyThread();
+		t.start();
+
+		for (int i = 0; i < 5; i++) {
+			// Control passes to child thread
+			Thread.yield();
+
+			// After execution of child Thread
+			// main thread takes over
+			System.out.println(Thread.currentThread().getName() + " in control");
+		}
+	}
 }
