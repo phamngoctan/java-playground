b4ca6e574c23a6b9994fa644438ce138c5f49aad
phamngoctan <pnt.dtqn1991@gmail.com>
2019-09-24 07:09:32 +0700

    add examples for join(), sleep() and yield()

---
+++ src/test/java/com/tanpham/playaround/thread/JoinExample.java
@@ -0,0 +1,25 @@
+package com.tanpham.playaround.thread;
+
+public class JoinExample implements Runnable {
+	public void run() {
+		Thread t = Thread.currentThread();
+		System.out.println("Current thread: " + t.getName());
+
+		// checks if current thread is alive
+		System.out.println("Is Alive? " + t.isAlive());
+	}
+
+	public static void main(String args[]) throws Exception {
+		Thread t = new Thread(new JoinExample());
+		t.start();
+
+		// Waits for 1000ms this thread to die.
+		t.join(1000);
+
+		System.out.println("\nJoining after 1000" + " mili seconds: \n");
+		System.out.println("Current thread: " + t.getName());
+
+		// Checks if this thread is alive
+		System.out.println("Is alive? " + t.isAlive());
+	}
+}
---
+++ src/test/java/com/tanpham/playaround/thread/YieldExample.java
@@ -0,0 +1,5 @@
+package com.tanpham.playaround.thread;
+
+public class YieldExample {
+
+}
