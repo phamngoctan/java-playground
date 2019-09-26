559f6ed8c1dd9bfc6b0ab3b9700ea5e0404e52bf
phamngoctan <pnt.dtqn1991@gmail.com>
2019-09-26 13:33:36 +0700

    add the rough implementation for pushFront method

--- src/main/java/com/tanpham/playaround/datastructure/MyLinkedListWithTail.java
+++ src/main/java/com/tanpham/playaround/datastructure/MyLinkedListWithTail.java
@@ -5,27 +5,47 @@ public class MyLinkedListWithTail<E> {
     private Node<E> head;
     private Node<E> tail;
     
+    private class Node<E> {
+        Node<E> next;
+        E value;
+        
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+    
     public int size() {
         return size;
     }
 
     public boolean empty() {
-        return true;
+        return size == 0;
     }
 
     public E valueAt(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
-        Node<E> next = head.next();
+        Node<E> next = head;
         int i = 0;
         while (next != null) {
             if (i == index) {
-                return next.value();
+                return next.value;
             }
             i++;
+            next = next.next;
         }
         return null;
+    }
+
+    public void pushFront(E value) {
+        Node<E> newNode = new Node<>(value);
+        if (head != null) {
+            newNode.next = head;
+        }
+        head = newNode;
+        
+        size++;
     }
     
 }
--- src/test/java/com/tanpham/playaround/datastructure/MyLinkedListWithTailTest.java
+++ src/test/java/com/tanpham/playaround/datastructure/MyLinkedListWithTailTest.java
@@ -30,4 +30,31 @@ public class MyLinkedListWithTailTest {
         linkedList.valueAt(0);
     }
     
+    @Test
+    public void pushFront_addItemToCompletelyNewLinkedList_sizeShouldBeOne() {
+        linkedList.pushFront("Java");
+        assertThat(linkedList.size(), equalTo(1));
+        assertThat(linkedList.valueAt(0), equalTo("Java"));
+    }
+    
+    @Test
+    public void pushFront_addItemToCompletelyNewLinkedList_itShouldBeNotEmpty() {
+        linkedList.pushFront("Java");
+        assertThat(linkedList.empty(), equalTo(false));
+    }
+    
+    @Test
+    public void pushFront_addItemToCompletelyNewLinkedList_valueAtZeroIndexShouldHaveValue() {
+        linkedList.pushFront("Java");
+        assertThat(linkedList.valueAt(0), equalTo("Java"));
+    }
+    
+    @Test
+    public void valueAt_addTwoItems_findTheSecondOne() {
+        linkedList.pushFront("Python");
+        linkedList.pushFront("Java");
+        assertThat(linkedList.size(), equalTo(2));
+        assertThat(linkedList.valueAt(1), equalTo("Python"));
+    }
+    
 }
