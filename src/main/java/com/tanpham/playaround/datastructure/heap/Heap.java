1166694641820ef45dbd340db7b5f37afdc1fdaa
phamngoctan <pnt.dtqn1991@gmail.com>
2019-12-02 07:23:26 +0700

    Add raw heap implementation

---
+++ src/main/java/com/tanpham/playaround/datastructure/heap/Heap.java
@@ -0,0 +1,14 @@
+package com.tanpham.playaround.datastructure.heap;
+
+import com.tanpham.playaround.datastructure.binarysearchtree.Node;
+
+public class Heap<T> {
+	private Node<T>[] arr;
+	
+	@SuppressWarnings("unchecked")
+	public Heap() {
+		arr = new Node[16];
+	}
+	
+	
+}
