38c6bd926be7e2db51ab7ec49a8c7a6aaf62f5bd
phamngoctan <pnt.dtqn1991@gmail.com>
2019-12-05 13:10:29 +0700

    Finish the implementation for building a max heap

--- src/main/java/com/tanpham/playaround/datastructure/heap/MaxHeap.java
+++ src/main/java/com/tanpham/playaround/datastructure/heap/MaxHeap.java
@@ -17,8 +17,6 @@ public class MaxHeap<T extends Comparable<? super T>> {
 		return arr;
 	}
 	
-	// TODO: add more method for the heap
-	
 	/**
 	 * Given a Binary Heap and a new element to be added to this Heap. <br/>
 	 * The task is to insert the new element to the Heap maintaining the properties of Heap.
@@ -32,30 +30,30 @@ public class MaxHeap<T extends Comparable<? super T>> {
 		// Special case: delete the root, swap with the last leaf
 	}
 	
-	public void heapify() {
+	/**
+	 * Build max heap from unordered array <br/>
+	 * 1. find max element A[1] <br/>
+	 * 2. swap element A[n] with A[1] <br/>
+	 *     now the max element is at the end of array<br/>
+	 * 3. discard node n from heap by decrementing heap size <br/>
+	 * 4. new root may violate max heap property but the child are max
+	 */
+	public void buildMaxHeap() {
 		// Original idea from Professor Srini Devadas
-//		int halfLength = arr.length/2;
-//		for (int i = halfLength; i >= 0; i--) {
-//			maxHeapify(arr, i);
-//		}
-		int halfLength = arr.length/2 - 1;
-//		System.out.println(halfLength);
-		for (int i = halfLength; i >= 0; i--) {
+		// From lastRootPosition to zero in the array are always the nodes which have sub tree. 
+		// Those nodes should be considered to be heapified.
+		int lastRootPosition = arr.length/2 - 1;
+		for (int i = lastRootPosition; i >= 0; i--) {
 			maxHeapify(arr, i);
 		}
 	}
 
+	// TODO: consider to implement it again :)
 	/**
 	 * Correct the subtree at position
 	 * @param atPosition
 	 */
 	public void maxHeapify(T[] visualizedArray, int atPosition) {
-//		visualizedArray[atPosition].getValue().equals(obj)
-//		if (visualizedArray[atPosition].getValue()) {
-//			
-//		}
-		System.out.println("Level " + atPosition);
-		
 		int max = visualizedArray.length;
 		int index = 0;
 		int leftChild = 0;
@@ -91,17 +89,4 @@ public class MaxHeap<T extends Comparable<? super T>> {
 		visualizedArray[secondItemIndex] = temp;
 	}
 	
-	/**
-	 * Build max heap from unordered array <br/>
-	 * 1. find max element A[1] <br/>
-	 * 2. swap element A[n] with A[1] <br/>
-	 *     now the max element is at the end of array<br/>
-	 * 3. discard node n from heap by decrementing heap size <br/>
-	 * 4. new root may violate max heap property but the child are max
-	 */
-	public void buildMaxHeap() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
