d605101c64e256f7683e20be2e6fca7a192350bd
phamngoctan <pnt.dtqn1991@gmail.com>
2019-12-09 07:24:58 +0700

    Improve the implementation of reversing a linkedlist

--- src/main/java/com/tanpham/playaround/datastructure/MyLinkedListWithTail.java
+++ src/main/java/com/tanpham/playaround/datastructure/MyLinkedListWithTail.java
@@ -223,27 +223,46 @@ public class MyLinkedListWithTail<E> {
 		return next.value;
 	}
 
+//    public void reverse() {
+//        if (size <= 1) {
+//            return;
+//        }
+//        
+//        Node<E> newLinkedListNode = null;
+//        Node<E> next = head;
+//        while (next != null) {
+//        	
+//        	Node<E> nodeToBeAdded = new Node<>(next.value);
+//        	if (newLinkedListNode == null) {
+//        		tail = nodeToBeAdded;
+//        	}
+//        	
+//        	nodeToBeAdded.next = newLinkedListNode;
+//        	newLinkedListNode = nodeToBeAdded;
+//    		
+//        	next = next.next;
+//        }
+//        head = newLinkedListNode;
+//    }
+    
     public void reverse() {
-        if (size <= 1) {
+        reverseUsingTwoPointer();
+    }
+
+    //TODO: continue to implement this
+	private void reverseUsingTwoPointer() {
+		if (size <= 1) {
             return;
         }
         
         Node<E> newLinkedListNode = null;
+        Node<E> prev = null;
         Node<E> next = head;
         while (next != null) {
         	
-        	Node<E> nodeToBeAdded = new Node<>(next.value);
-        	if (newLinkedListNode == null) {
-        		tail = nodeToBeAdded;
-        	}
-        	
-        	nodeToBeAdded.next = newLinkedListNode;
-        	newLinkedListNode = nodeToBeAdded;
-    		
-        	next = next.next;
         }
         head = newLinkedListNode;
-    }
+	}
 	
 	//Not up-to-date like erase method
 	public void delete(int index) {
