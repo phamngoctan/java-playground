1a52a94867e94cd5280fc387f5ce4cb1d53d7ac6
phamngoctan <pnt.dtqn1991@gmail.com>
2019-12-10 13:33:52 +0700

    Add the instruction for implementing the merge sort

--- src/main/java/com/tanpham/playaround/sorting/MergeSort.java
+++ src/main/java/com/tanpham/playaround/sorting/MergeSort.java
@@ -13,7 +13,16 @@ public class MergeSort<T> {
 			return inputHead;
 		}
 		
+		// Idea to implement the Merge sort
+		// 1: split the linkedlist into two parts
+		// 2: call recursively the proceed for first part & second part
+		// 3: merge the two sorted parts & return the sorted linkedlist
+		
 		return null;
+	}
+	
+	public Node<T> mergeTwoSortedList(Node<T> firstHead, Node<T> secondHead) {
+		return firstHead;
 	}
 	
 }
--- src/test/java/com/tanpham/playaround/sorting/MergeSortTest.java
+++ src/test/java/com/tanpham/playaround/sorting/MergeSortTest.java
@@ -29,6 +29,15 @@ public class MergeSortTest {
 	}
 	
 	@Test
+	public void mergeSort__twoNodesToFacilitateTheRecursiveCall() {
+		Node<Integer> head = new Node<>(10);
+		head.setNext(15);
+		Node<Integer> proceededList = mergeSort.proceed(head);
+		assertThat(proceededList.getValue(), Matchers.equalTo(15));
+		assertThat(proceededList.getNext(), Matchers.equalTo(10));
+	}
+	
+	@Test
 	public void mergeSort__multipleNodes() {
 		Node<Integer> head = new Node<>(10);
 		head.setNext(20).setNext(5);
