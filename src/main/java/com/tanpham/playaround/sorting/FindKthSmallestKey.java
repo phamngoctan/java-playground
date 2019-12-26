fb7e7148533ca5798ac73b3af96ad048454ec9db
phamngoctan <pnt.dtqn1991@gmail.com>
2019-12-26 07:25:30 +0700

    Add classes for Quicksort with duplicate keys handling

---
+++ src/main/java/com/tanpham/playaround/sorting/FindKthSmallestKey.java
@@ -0,0 +1,13 @@
+package com.tanpham.playaround.sorting;
+
+/**
+ * The idea is that we apply the quick sort partition approach. Each time we finish a partition process, we pick the part that
+ * Kth can be located in
+ */
+public class FindKthSmallestKey {
+
+	public static void findIt(int[] arr, int kth) {
+		
+	}
+	
+}
---
+++ src/main/java/com/tanpham/playaround/sorting/QuickSortWithDuplicateKeysHandling.java
@@ -0,0 +1,23 @@
+package com.tanpham.playaround.sorting;
+
+/*
+ * For the sake of the idea implementation, this class will not support generic type
+ */
+public class QuickSortWithDuplicateKeysHandling {
+
+	public void proceed(int[] arr) {
+		QuickSort.shuffleArray(arr);
+		quickSort(arr, 0, arr.length);
+	}
+
+	private void quickSort(int[] arr, int low, int high) {
+		int partitionPoint = partition(arr, low, high);
+		quickSort(arr, 0, partitionPoint - 1);
+		quickSort(arr, partitionPoint + 1, high);
+	}
+
+	private int partition(int[] arr, int low, int high) {
+		return 0;
+	}
+	
+}
