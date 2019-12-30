e639fc64f2baf86323e6c19183f66ca1f6ca1f9b
phamngoctan <pnt.dtqn1991@gmail.com>
2019-12-30 13:35:35 +0700

    Add the implementation and test for quicksort with duplicate keys handling

--- src/main/java/com/tanpham/playaround/sorting/QuickSortWithDuplicateKeysHandling.java
+++ src/main/java/com/tanpham/playaround/sorting/QuickSortWithDuplicateKeysHandling.java
@@ -6,18 +6,29 @@ package com.tanpham.playaround.sorting;
 public class QuickSortWithDuplicateKeysHandling {
 
 	public void proceed(int[] arr) {
+		if (arr == null) {
+			return;
+		}
 		QuickSort.shuffleArray(arr);
-		quickSort(arr, 0, arr.length);
+		quickSort(arr, 0, arr.length - 1);
 	}
 
 	private void quickSort(int[] arr, int low, int high) {
-		int partitionPoint = partition(arr, low, high);
-		quickSort(arr, 0, partitionPoint - 1);
-		quickSort(arr, partitionPoint + 1, high);
+		if (low >= high) {
+			return;
+		}
+		NextBoundary nextBoundary = partition(arr, low, high);
+		quickSort(arr, 0, nextBoundary.lowBoundary);
+		quickSort(arr, nextBoundary.highBoundary, high);
 	}
 
-	//TODO: implement it
-	public int partition(int[] arr, int low, int high) {
+	/**
+	 * There are three posibility for the changes inside one loop <br/>
+	 * first: if lower, swap lt vs i, i++, lt++ <br/>
+	 * second: if higher, swap i vs gt, gt--    <br/>
+	 * third: if equal, i++ (always happens in the first time of the loop when i = lt) <br/>
+	 */
+	public NextBoundary partition(int[] arr, int low, int high) {
 		int i = low;
 		int lt = low;
 		int gt = high;
@@ -34,7 +45,21 @@ public class QuickSortWithDuplicateKeysHandling {
 				i++;
 			}
 		}
-		return lt;
+		NextBoundary nextBoundary = new NextBoundary();
+		nextBoundary.lowBoundary = lt - 1;
+		nextBoundary.highBoundary = i;
+		return nextBoundary;
+	}
+	
+	class NextBoundary {
+		int lowBoundary;
+		int highBoundary;
+		public int getLowBoundary() {
+			return lowBoundary;
+		}
+		public int getHighBoundary() {
+			return highBoundary;
+		}
 	}
 	
 }
--- src/test/java/com/tanpham/playaround/sorting/QuickSortWithDuplicateKeysHandlingTest.java
+++ src/test/java/com/tanpham/playaround/sorting/QuickSortWithDuplicateKeysHandlingTest.java
@@ -6,6 +6,8 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tanpham.playaround.sorting.QuickSortWithDuplicateKeysHandling.NextBoundary;
+
 public class QuickSortWithDuplicateKeysHandlingTest {
 	
 	private QuickSortWithDuplicateKeysHandling quickSort;
@@ -53,10 +55,170 @@ public class QuickSortWithDuplicateKeysHandlingTest {
 	@Test
 	public void withoutDuplicatedKeys__butTheEndItemIsLargest__itDoesNotMatter() {
 		int[] arr = new int[] {5, 3, 8, 6, 1, 9};
-		int nextPoint = quickSort.partition(arr, 0, 5);
+		NextBoundary nextBoundary = quickSort.partition(arr, 0, 5);
 		
-		assertThat(nextPoint, Matchers.equalTo(2));
+		assertThat(nextBoundary.lowBoundary, Matchers.equalTo(1));
+		assertThat(nextBoundary.highBoundary, Matchers.equalTo(3));
 		assertThat(arr, Matchers.equalTo(new int[] {3, 1, 5, 6, 9, 8}));
+	}
+	
+	/*
+	 * initiating:
+	 * i
+	 * lt                       gt
+	 * [5, 3, 7, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * first loop:
+	 *     i
+	 * lt                       gt
+	 * [5, 3, 7, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * second loop:
+	 *        i
+	 *     lt                   gt
+	 * [3, 5, 7, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * third loop:
+	 *        i
+	 *     lt                 gt
+	 * [3, 5, 10, 8, 6, 7, 9, 7, 7]
+	 * 
+	 * 4th loop:
+	 *        i
+	 *     lt                 gt
+	 * [3, 5, 10, 8, 6, 7, 9, 7, 7]
+	 * 
+	 * 5th loop:
+	 *        i
+	 *     lt             gt
+	 * [3, 5, 7, 8, 6, 7, 9, 10, 7]
+	 * 
+	 * 6th loop:
+	 *        i
+	 *     lt          gt
+	 * [3, 5, 9, 8, 6, 7, 7, 10, 7]
+	 * 
+	 * 7th loop:
+	 *        i
+	 *     lt       gt
+	 * [3, 5, 7, 8, 6, 9, 7, 10, 7]
+	 * 
+	 * 8th loop:
+	 *        i
+	 *     lt    gt
+	 * [3, 5, 6, 8, 7, 9, 7, 10, 7]
+	 * 
+	 * 9th loop:
+	 *        i
+	 *     lt gt
+	 * [3, 5, 8, 6, 7, 9, 7, 10, 7]
+	 * 
+	 * 10th loop: swap i, gt means 8 vs 8
+	 *     gt i
+	 *     lt 
+	 * [3, 5, 8, 6, 7, 9, 7, 10, 7]
+	 * 
+	 * stop
+	 * 
+	 */
+	@Test
+	public void withDuplicatedKeys__butDuplicatedKeyIsNotTheComparedPoint() {
+		int arr[] = new int[] {5, 3, 7, 8, 6, 7, 9, 7, 10};
+		
+		NextBoundary nextBoundary = quickSort.partition(arr, 0, 8);
+		
+		assertThat(nextBoundary.lowBoundary, Matchers.equalTo(0));
+		assertThat(nextBoundary.highBoundary, Matchers.equalTo(2));	
+		assertThat(arr, Matchers.equalTo(new int[] {3, 5, 8, 6, 7, 9, 7, 10, 7}));
+	}
+	
+	/*
+	 * initiating:
+	 * i
+	 * lt                       gt
+	 * [7, 3, 5, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * first loop: always increase i without changing anything :)
+	 *     i
+	 * lt                       gt
+	 * [7, 3, 5, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * 2nd loop:
+	 *     i
+	 * lt                       gt
+	 * [7, 3, 5, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * 3rd loop:
+	 *        i
+	 *     lt                   gt
+	 * [3, 7, 5, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * 4th loop:
+	 *           i
+	 *        lt                gt
+	 * [3, 5, 7, 8, 6, 7, 9, 7, 10]
+	 * 
+	 * 5th loop:
+	 *           i
+	 *        lt              gt
+	 * [3, 5, 7, 10, 6, 7, 9, 7, 8]
+	 * 
+	 * 6th loop: just increase i, no exchange needed
+	 *              i
+	 *        lt          gt
+	 * [3, 5, 7, 7, 6, 7, 9, 10, 8]
+	 * 
+	 * 7th loop:
+	 *                 i
+	 *           lt       gt
+	 * [3, 5, 6, 7, 7, 7, 9, 10, 8]
+	 * 
+	 * 8th loop: just increase i, no exchange needed
+	 *                    i
+	 *           lt       gt
+	 * [3, 5, 6, 7, 7, 7, 9, 10, 8]
+	 * 
+	 * 9th loop: swap i vs gt means 9 vs 9, decrease gt
+	 *                    i
+	 *           lt    gt
+	 * [3, 5, 6, 7, 7, 7, 9, 10, 8]
+	 * 
+	 * stop
+	 * 
+	 */
+	@Test
+	public void withDuplicatedKeys__duplicatedKeyIsTheComparedPoint() {
+		int arr[] = new int[] {7, 3, 5, 8, 6, 7, 9, 7, 10};
+		
+		NextBoundary nextBoundary = quickSort.partition(arr, 0, 8);
+		
+		assertThat(nextBoundary.lowBoundary, Matchers.equalTo(2));
+		assertThat(nextBoundary.highBoundary, Matchers.equalTo(6));	
+		assertThat(arr, Matchers.equalTo(new int[] {3, 5, 6, 7, 7, 7, 9, 10, 8}));
+	}
+	
+	@Test
+	public void quickSort__proceed() {
+		int arr[] = new int[] {7, 3, 5, 8, 6, 7, 9, 7, 10};
+		
+		quickSort.proceed(arr);
+		assertThat(arr, Matchers.equalTo(new int[] {3, 5, 6, 7, 7, 7, 8, 9, 10}));
+	}
+	
+	@Test
+	public void quickSort__oneItem() {
+		int arr[] = new int[] {7};
+		
+		quickSort.proceed(arr);
+		assertThat(arr, Matchers.equalTo(new int[] {7}));
+	}
+	
+	/*
+	 * Nothing happens
+	 */
+	@Test
+	public void quickSort__nullArr() {
+		quickSort.proceed(null);
 	}
 	
 }
