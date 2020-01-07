1065316bfe5b8a72a22071cd18880b5f3a6703f2
phamngoctan <pnt.dtqn1991@gmail.com>
2020-01-07 07:29:57 +0700

    Add the prototype for handling DayOneSuffixStructure

---
+++ src/main/java/com/tanpham/playaround/algorithm/DayOneSuffixStructure.java
@@ -0,0 +1,22 @@
+package com.tanpham.playaround.algorithm;
+
+import java.util.Scanner;
+
+public class DayOneSuffixStructure {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		sc.close();
+		System.out.println(getAnswer(s1, s2));
+	}
+	
+	public static String getAnswer(String s1, String s2) {
+		char[] arr1 = s1.toCharArray();
+		char[] arr2 = s2.toCharArray();
+		
+		return null;
+	}
+
+}
