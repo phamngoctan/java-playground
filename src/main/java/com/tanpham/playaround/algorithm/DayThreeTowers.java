2760656b5bbea102f8feef61736890a6ef7355d9
phamngoctan <pnt.dtqn1991@gmail.com>
2020-01-13 07:26:11 +0700

    Add imlementation for day three tower exercise

---
+++ src/main/java/com/tanpham/playaround/algorithm/DayThreeTowers.java
@@ -0,0 +1,64 @@
+package com.tanpham.playaround.algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DayThreeTowers {
+
+	static class Tower {
+		private int height;
+		private int total;
+		public int getHeight() {
+			return height;
+		}
+		public void setHeight(int height) {
+			this.height = height;
+		}
+		public int getTotal() {
+			return total;
+		}
+		public void setTotal(int total) {
+			this.total = total;
+		}
+		public Tower(int height, int total) {
+			super();
+			this.height = height;
+			this.total = total;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int length = sc.nextInt();
+		int[] arr = new int[length];
+		for(int i = 0; i < length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		Tower tower = getTower(arr);
+		System.out.print(tower.height + " " + tower.total);
+	}
+	
+	public static Tower getTower(int[] arr) {
+		Arrays.sort(arr);
+		int maxHeight = -1;
+		int curHeight = 1;
+		int total = 0;
+		for (int i = arr.length - 1; i >= 1; i--) {
+			if (arr[i] == arr[i - 1]) {
+				curHeight++;
+			} else {
+				if (curHeight > maxHeight) {
+					maxHeight = curHeight;
+					total = arr[i];
+				}
+			}
+		}
+		
+		if (maxHeight == -1) {
+			return new Tower(curHeight, arr[0]);
+		}
+		return new Tower(maxHeight, total);
+	}
+	
+}
---
+++ src/test/java/com/tanpham/playaround/algorithm/DayThreeTowersTest.java
@@ -0,0 +1,36 @@
+package com.tanpham.playaround.algorithm;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import com.tanpham.playaround.algorithm.DayThreeTowers.Tower;
+
+public class DayThreeTowersTest {
+
+	@Test
+	public void myTestCase01() {
+		int[] arr = new int[] {7, 7, 7};
+		Tower tower = DayThreeTowers.getTower(arr);
+		assertThat(tower.getHeight(), Matchers.equalTo(3));
+		assertThat(tower.getTotal(), Matchers.equalTo(7));
+	}
+	
+	@Test
+	public void testCase01() {
+		int[] arr = new int[] {1, 2, 3};
+		Tower tower = DayThreeTowers.getTower(arr);
+		assertThat(tower.getHeight(), Matchers.equalTo(1));
+		assertThat(tower.getTotal(), Matchers.equalTo(3));
+	}
+	
+	@Test
+	public void testCase02() {
+		int[] arr = new int[] {6, 5, 6, 7};
+		Tower tower = DayThreeTowers.getTower(arr);
+		assertThat(tower.getHeight(), Matchers.equalTo(2));
+		assertThat(tower.getTotal(), Matchers.equalTo(12));
+	}
+	
+}
