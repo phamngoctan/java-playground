a7e0c90e555d01ec9434593450d37023233f95f7
phamngoctan <pnt.dtqn1991@gmail.com>
2020-01-22 11:32:19 +0700

    Add structure for leetcode issue 560

---
+++ src/main/java/com/tanpham/playaround/leetcode/L560.java
@@ -0,0 +1,10 @@
+package com.tanpham.playaround.leetcode;
+
+public class L560 {
+
+	public static int subarraySum(int[] nums, int k) {
+		int result = 0;
+		return result;
+	}
+
+}
---
+++ src/test/java/com/tanpham/playaround/leetcode/L560Test.java
@@ -0,0 +1,13 @@
+package com.tanpham.playaround.leetcode;
+
+import org.junit.Test;
+
+public class L560Test {
+
+	@Test
+	public void testCase01() {
+		int[] nums = new int[] {1,1,1};
+		L560.subarraySum(nums, 2);
+	}
+	
+}
