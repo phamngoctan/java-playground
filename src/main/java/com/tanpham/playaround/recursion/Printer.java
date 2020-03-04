3c50de48ef3baa66a92b7b424ff8f24d2962120e
phamngoctan <pnt.dtqn1991@gmail.com>
2020-03-04 13:35:14 +0700

    Add more method using recursive approach

--- src/main/java/com/tanpham/playaround/recursion/Printer.java
+++ src/main/java/com/tanpham/playaround/recursion/Printer.java
@@ -3,7 +3,8 @@ package com.tanpham.playaround.recursion;
 public class Printer {
 	
 	public static void main(String[] args) {
-		printSeries(5, 10);
+//		printSeries(5, 10);
+		System.out.println(sum(3));
 	}
 
 	public static void printSeries(int from, int to) {
@@ -13,6 +14,18 @@ public class Printer {
 		} else {
 			System.out.println(from);
 			printSeries(from + 1, to);
+		}
+	}
+	
+	/*
+	 * sum(6) = 6 + sum(5)
+		      = 6 + 5 + sum(4)
+	 */
+	public static int sum(int n) {
+		if (n <= 0) {
+			return 0;
+		} else {
+			return n + sum(n - 1);
 		}
 	}
 	
