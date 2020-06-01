6c4440d6aec43eb37246e1218a88ead07cea5326
phamngoctan <pnt.dtqn1991@gmail.com>
2020-06-01 17:59:36 +0700

    add solution for jump game L531

---
+++ src/main/java/com/tanpham/playaround/leetcode/L531JumpGame.java
@@ -0,0 +1,40 @@
+package com.tanpham.playaround.leetcode;
+
+public class L531JumpGame {
+
+}
+
+/**
+ * Timeout, not good because it takes O(n^2)
+ */
+//class Solution {    
+//    public boolean canJump(int[] nums) {
+//        return reachTarget(nums, 0, nums[0]);
+//    }
+//    
+//    private boolean reachTarget(int[] nums, int start, int numJumpStep) {
+//        if (start >= nums.length - 1) {
+//            return true;
+//        }
+//        
+//        for (int j = 1; j <= numJumpStep; j++) {
+//            if (reachTarget(nums, start + j, nums[start + j])) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
+
+class Solution {
+	public boolean canJump(int[] nums) {
+		int last = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (i > last) {
+				return false;
+			}
+			last = Math.max(last, nums[i] + i);
+		}
+		return last == nums.length;
+	}
+}
