077ed3be655656291307baf1aa4aa0607533a878
phamngoctan <pnt.dtqn1991@gmail.com>
2020-06-13 13:29:38 +0700

    Add the notes for day 8

---
+++ src/main/java/com/tanpham/playaround/algorithm/Day8Note.java
@@ -0,0 +1,12 @@
+package com.tanpham.playaround.algorithm;
+
+/**
+ * 
+ * Dijkstra
+ * 
+ * When implementation, please check the one-way direction or two-ways. Otherwise, first exercise is red :)
+ *
+ */
+public class Day8Note {
+
+}
