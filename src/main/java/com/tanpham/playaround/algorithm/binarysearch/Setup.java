203d6cf998696ecd4522a34ca17f3ff32e6c0e0d
phamngoctan <pnt.dtqn1991@gmail.com>
2020-06-27 10:50:13 +0700

    Add note for binary search lesson

---
+++ src/main/java/com/tanpham/playaround/algorithm/binarysearch/Setup.java
@@ -0,0 +1,43 @@
+package com.tanpham.playaround.algorithm.binarysearch;
+
+/**
+ * NOTES:
+ * - overflow when calculating mid
+ * - binary search with first matched value
+ * - binary search with last matched value
+ * - lower bound
+ * - upper bound
+ */
+public class Setup {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {
+				1, 2, 5, 9, 10, 100, 1000, 1400
+		};
+		int left = 0;
+		int right = arr.length;
+		int res = binarySearch(arr, left, right, 2);
+		if (res == -1) {
+			System.out.println("Not found!");
+		} else {
+			System.out.println("Found");
+		}
+	}
+
+	private static int binarySearch(int[] arr, int left, int right, int valueToBeChecked) {
+		int mid;
+		while (left <= right) {
+			//mid = (left + right) / 2; // NONO: 20 years to be fixed, overflow possibly
+			mid = left + (right - left) / 2;
+			if (valueToBeChecked < arr[mid]) {
+				right = mid;
+			} else if (valueToBeChecked == arr[mid]) {
+				return mid;
+			} else {
+				left = mid;
+			}
+		}
+		return -1;
+	}
+	
+}
