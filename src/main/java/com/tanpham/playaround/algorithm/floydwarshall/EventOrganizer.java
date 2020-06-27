da48e40bbca762757e8f84b6689e1b8e0442f364
phamngoctan <pnt.dtqn1991@gmail.com>
2020-06-27 10:05:46 +0700

    Add classes for floyd warshall

---
+++ src/main/java/com/tanpham/playaround/algorithm/floydwarshall/EventOrganizer.java
@@ -0,0 +1,19 @@
+package com.tanpham.playaround.algorithm.floydwarshall;
+
+import java.util.Scanner;
+
+public class EventOrganizer {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCases = sc.nextInt();
+		for (int i = 0; i < testCases; i++) {
+			handleATestCase(sc);
+		}
+	}
+
+	private static void handleATestCase(Scanner sc) {
+		
+	}
+	
+}
---
+++ src/main/java/com/tanpham/playaround/algorithm/floydwarshall/GregAndGraph.java
@@ -0,0 +1,15 @@
+package com.tanpham.playaround.algorithm.floydwarshall;
+
+import java.util.Scanner;
+
+public class GregAndGraph {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int V = sc.nextInt();
+		for (int i = 0; i < V; i++) {
+			
+		}
+	}
+	
+}
