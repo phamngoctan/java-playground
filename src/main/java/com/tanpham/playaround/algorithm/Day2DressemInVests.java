b1b990da69b773deee3fbecf637763972f68cb6d
phamngoctan <pnt.dtqn1991@gmail.com>
2020-07-25 09:59:55 +0700

    Refactor code

--- src/main/java/com/tanpham/playaround/algorithm/Day2DressemInVests.java
+++ src/main/java/com/tanpham/playaround/algorithm/Day2DressemInVests.java
@@ -6,6 +6,40 @@ import java.util.Scanner;
 
 public class Day2DressemInVests {
 	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int[] arrN = new int[n];
+		for (int i = 0; i < n; i++) {
+			arrN[i] = sc.nextInt();
+		}
+		int[] arrM = new int[m];
+		for (int i = 0; i < m; i++) {
+			arrM[i] = sc.nextInt();
+		}
+		
+		List<String> res = new ArrayList<>();
+		int j = 0;
+		for (int i = 0; i < n && j < m;) {
+			if (arrN[i] - x <= arrM[j] && arrN[i] + y >= arrM[j]) {
+				res.add((i + 1) + " " + (j + 1));
+				j++;
+				i++;
+			} else if (arrN[i] + y < arrM[j]) {
+				i++;
+			} else if (arrN[i] - x > arrM[j]) {
+				j++;
+			}
+		}
+		System.out.println(res.size());
+		for (String i : res) {
+			System.out.println(i);
+		}
+	}
+	
 	static class Pair {
 		private int solderNumber;
 		private int vestNumber;
@@ -28,30 +62,30 @@ public class Day2DressemInVests {
 		}
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int soldierLength = sc.nextInt();
-		int vestLength = sc.nextInt();
-		int lowerBound = sc.nextInt();
-		int higherBound = sc.nextInt();
-		
-		int[] soldierArr = new int[soldierLength];
-		for (int i = 0; i < soldierLength; i++) {
-			soldierArr[i] = sc.nextInt();
-		}
-		
-		int[] vestArr = new int[vestLength];
-		for (int i = 0; i < vestLength; i++) {
-			vestArr[i] = sc.nextInt();
-		}
-		sc.close();
-		
-		List<Pair> result = check(soldierArr, vestArr, lowerBound, higherBound);
-		System.out.println(result.size());
-		for (Pair pair : result) {
-			System.out.println(pair.getSolderNumber() + " " + pair.getVestNumber());
-		}
-	}
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int soldierLength = sc.nextInt();
+//		int vestLength = sc.nextInt();
+//		int lowerBound = sc.nextInt();
+//		int higherBound = sc.nextInt();
+//		
+//		int[] soldierArr = new int[soldierLength];
+//		for (int i = 0; i < soldierLength; i++) {
+//			soldierArr[i] = sc.nextInt();
+//		}
+//		
+//		int[] vestArr = new int[vestLength];
+//		for (int i = 0; i < vestLength; i++) {
+//			vestArr[i] = sc.nextInt();
+//		}
+//		sc.close();
+//		
+//		List<Pair> result = check(soldierArr, vestArr, lowerBound, higherBound);
+//		System.out.println(result.size());
+//		for (Pair pair : result) {
+//			System.out.println(pair.getSolderNumber() + " " + pair.getVestNumber());
+//		}
+//	}
 	
 	/**
 	 * If you draw it out, it is easier to visualize the code. Otherwise you may get lost
