a611b1451ad9a19a7b40d66944d2893637aec84e
phamngoctan <pnt.dtqn1991@gmail.com>
2020-07-30 07:28:32 +0700

    Add raw implementation for finding path in binary matrix

---
+++ src/main/java/com/tanpham/playaround/algorithm/bfs/ShortestPathInBinaryMatrix.java
@@ -0,0 +1,57 @@
+package com.tanpham.playaround.algorithm.bfs;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class ShortestPathInBinaryMatrix {
+	private int MAX = 105;
+	private boolean visited[][] = new boolean[MAX][MAX];
+	Pair destination;
+	int[][] direction = new int[][]{
+		{-1,-1},
+		{0,-1},
+		{1,-1},
+		{1,0},
+		{1,1},
+		{0,1},
+		{-1,1},
+		{-1,0}
+		};
+	
+    public boolean shortestPathBinaryMatrix(int[][] grid) {
+		destination = new Pair(grid.length, grid[0].length);
+		return BFS(grid, new Pair(0,0));
+    }
+	
+	static class Pair {
+		public int x;
+		public int y;
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	private boolean BFS(int[][] grid, Pair s) {
+		Queue<Pair> q = new LinkedList<>();
+		q.add(s);
+		visited[s.x][s.y] = true;
+		while (!q.isEmpty()) {
+			Pair cur = q.poll();
+			if (cur.x == destination.x && cur.y == destination.y) {
+				return true;
+			}
+			visited[cur.x][cur.y] = true;
+			for (int i = 0; i < direction.length;i++) {
+				int newX = cur.x + direction[i][0];
+				int newY = cur.y + direction[i][1];
+				if (newX >= 0 
+					&& newY <= destination.y
+					&& !visited[newX][newY]
+					&& grid[newX][newY] == 0) {
+					q.add(new Pair(newX, newY));
+				}
+			}
+		}
+		return false;
+ 	}
+}
